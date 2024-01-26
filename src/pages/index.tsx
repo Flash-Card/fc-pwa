00c4e07a15514328daea4ddf5cf1c467496b4489
agolovchuk <a.golovchuk@gmail.com>
2024-01-26 18:21:04 +0100

    set default route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import { memo } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Layout } from 'pages/common/layout';
-import { Decks, CreateDeck, Deck } from 'pages/decks';
+import { memo } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "pages/common/layout";
+import { Decks, CreateDeck, Deck } from "pages/decks";
 
 const App = () => {
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<Decks />} />
-        <Route path='deck/add' element={<CreateDeck />} />
-        <Route path='deck/:id/*' element={<Deck />} />
+        <Route path="deck/add" element={<CreateDeck />} />
+        <Route path="deck/:id/*" element={<Deck />} />
+        <Route path="*" element={<Decks />} />
       </Routes>
     </Layout>
-  )
-}
+  );
+};
 
 export default memo(App);
